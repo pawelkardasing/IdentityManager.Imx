23bc036f3eaa61e1f9dc9e70e119b66d876f3b24
Adam Hecht <adamo156@gmail.com>
2023-09-22 10:36:50 +0200

    Added API connection for receiving data

--- imxweb/projects/qer/src/lib/support/newsletter/newsletter.component.ts
+++ imxweb/projects/qer/src/lib/support/newsletter/newsletter.component.ts
@@ -1,33 +1,54 @@
 import { Component } from '@angular/core';
+import { MethodDescriptor, TimeZoneInfo } from 'imx-qbm-dbts';
+import { AppConfigService, AuthenticationService } from 'qbm';
+
+export interface ReleaseInterface {
+  title: string;
+  url: string;
+  release_name: string;
+  date: string;
+}
+
+const ReleasesData: ReleaseInterface[] = [
+  {title: '', url: '', release_name: '', date: ''},
+];
+
 
 @Component({
   selector: 'ccc-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.scss']
 })
+
 export class NewsletterComponent {
   
-  releases: any[] = [
-    {
-      title: "Operational information - 1",
-      url: "ING HUBS/IAM/IG/1",
-      release_name: "https://confluence.ing.net/IAMHubs/1",
-      date: "21/09/2023 13:37:01",
-      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
+  _ReleasesData = ReleasesData;
+
+  constructor(
+    private readonly config: AppConfigService,
+    private readonly authentication: AuthenticationService
+  ) {}
+  
+  public async ngOnInit(): Promise<void> {
+    this.authentication.update();
+    this.getApiData();
+  }
+
+ public async getApiData(): Promise<void> {
+  const data: any = await this.config.apiClient.processRequest(this.buildGetObject());
+  this._ReleasesData = data
+ }
+
+ private buildGetObject(): MethodDescriptor<ReleaseInterface> {
+  return {
+    path: `/portal/GetNews`,
+    method: 'GET',
+    headers: {
+      'imx-timezone': TimeZoneInfo.get(),
     },
-    {
-      title: "Operational information - 1",
-      url: "ING HUBS/IAM/IG/1",
-      release_name: "https://confluence.ing.net/IAMHubs/1",
-      date: "21/09/2023 13:37:01",
-      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
-    },
-    {
-      title: "Operational information - 1",
-      url: "ING HUBS/IAM/IG/1",
-      release_name: "https://confluence.ing.net/IAMHubs/1",
-      date: "21/09/2023 13:37:01",
-      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
-    }
-  ];
+    credentials: 'include',
+    observe: 'response',
+    responseType: 'json',
+    };
+  }
 }

b7cbbde2b8487d4d5eeb2cbff7f33102920e140b
Adam Hecht <adamo156@gmail.com>
2023-09-22 08:49:17 +0200

    Added list of releases to newsletter component (dummy data)

--- imxweb/projects/qer/src/lib/support/newsletter/newsletter.component.ts
+++ imxweb/projects/qer/src/lib/support/newsletter/newsletter.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccc-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.scss']
 })
-export class NewsletterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NewsletterComponent {
+  
+  releases: any[] = [
+    {
+      title: "Operational information - 1",
+      url: "ING HUBS/IAM/IG/1",
+      release_name: "https://confluence.ing.net/IAMHubs/1",
+      date: "21/09/2023 13:37:01",
+      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
+    },
+    {
+      title: "Operational information - 1",
+      url: "ING HUBS/IAM/IG/1",
+      release_name: "https://confluence.ing.net/IAMHubs/1",
+      date: "21/09/2023 13:37:01",
+      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
+    },
+    {
+      title: "Operational information - 1",
+      url: "ING HUBS/IAM/IG/1",
+      release_name: "https://confluence.ing.net/IAMHubs/1",
+      date: "21/09/2023 13:37:01",
+      description: "fhsdlagfdsj fgh sdlf gsdlfg sdkh fksdg fksagdfj sagdkjfgdskafjhds afhjdask fjhdksafguy"
+    }
+  ];
 }
